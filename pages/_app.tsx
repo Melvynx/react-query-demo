b4fe24a96f31091dcaa8b017e4a148aa804e332a
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-03-27 10:22:35 +0200

    reset app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,15 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import '~/styles/globals.css';
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <BackButton />
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <Component {...pageProps} />
+      <ReactQueryDevtools />
     </div>
   );
 }
